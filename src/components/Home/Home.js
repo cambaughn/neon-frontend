e85036437c44bacb1786bc53e97f080cbbc94141
Cameron Baughn <cam.baughn@gmail.com>
2017-11-16 11:28:32 -0800

    Fix auth

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import Dashboard from '../Dashboard/Dashboard';
 import './Home.css';
+
+import { isLoggedIn } from '../../util/auth/AuthService';
 
 const Home = () => {
   return (
     <div className={'home-container'}>
-      <h2>Welcome</h2>
-      <h3>Neon is the best way for you and your team to create and share your ideas</h3>
+      { isLoggedIn() ? (
+        <Dashboard />
+      ) : (
+        <div>
+          <h2>Welcome</h2>
+          <h3>Neon is the best way for you and your team to create and share your ideas</h3>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,21 +4,30 @@ import { Link } from 'react-router-dom';
 
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ login, logout, isLoggedIn }) => {
   return (
     <div className={'navbar'}>
       <Link to={'/'} className={'brand'}>
         <h2>Neon</h2>
       </Link>
 
-      <div className={'links'}>
-        <Link to={'/dashboard'} className={'link'}>
-          <p>Dashboard</p>
-        </Link>
-        <Link to={'/doc'} className={'link'}>
-          <p>Document</p>
-        </Link>
-      </div>
+      { isLoggedIn() &&
+        <div className={'links'}>
+          <Link to={'/'} className={'link'}>
+            <p>Dashboard</p>
+          </Link>
+          <Link to={'/doc'} className={'link'}>
+            <p>Document</p>
+          </Link>
+          <p className='link' onClick={() => logout()}>Log Out</p>
+        </div>
+      }
+
+      { !isLoggedIn() &&
+        <div className='links'>
+          <p className='link' onClick={() => login()}>Log In</p>
+        </div>
+      }
     </div>
   )
 }
